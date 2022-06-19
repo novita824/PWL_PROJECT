5d53fb6e6593ee8afd5781509cf8d21bb314c605
novita824 <snovita819@gmail.com>
2022-06-19 06:53:48 -0700

    change template siswa

--- resources/views/siswa/template.blade.php
+++ resources/views/siswa/template.blade.php
@@ -73,13 +73,13 @@ Coded by www.creative-tim.com
             </a>
           </li>
           <li>
-            <a href="./notifications.html">
+            <a href="./matkul">
               <i class="nc-icon nc-bell-55"></i>
               <p>Data Mata Pelajaran</p>
             </a>
           </li>
           <li>
-            <a href="./user.html">
+            <a href="./jurusan">
               <i class="nc-icon nc-single-02"></i>
               <p>Data Jurusan</p>
             </a>
