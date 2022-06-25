8a9185b5d01349f725bcab4bff3e698155e44049
novita824 <snovita819@gmail.com>
2022-06-25 04:39:45 -0700

    change controller mapel

--- app/Http/Controllers/MapelController.php
+++ app/Http/Controllers/MapelController.php
@@ -10,7 +10,7 @@ class MapelController extends Controller
     public function index()
     {
         $mapel = Mapel::all();
-        $posts = Mapel::orderBy('Nama', 'desc')->paginate(6);
+        $posts = Mapel::orderBy('Hari', 'desc')->paginate(6);
         return view('mapel.index', compact('mapel'))
         ->with('paginate', (request()->input('page', 1) - 1) * 5);
     }
@@ -23,54 +23,42 @@ class MapelController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'Nama' => 'required',
-            'Tugas1' => 'required',
-            'Tugas2' => 'required',
-            'Tugas3' => 'required',
-            'UTS' => 'required',
-            'Tugas4' => 'required',
-            'Tugas5' => 'required',
-            'Tugas6' => 'required',
-            'UAS' => 'required',
+            'Hari' => 'required',
+            'NamaMapel' => 'required',
+            'GuruPengampu' => 'required',
         ]);
         Mapel::create($request->all());
         return redirect()->route('mapel.index')
             ->with('success', 'Mata Pelajaran Berhasil Ditambahkan');
     }
 
-    public function show($nama)
+    public function show($hari)
     {
-        $mapel = Mapel::find($nama);
+        $mapel = Mapel::find($hari);
         return view('nama.detail', compact('mapel'));
     }
     
-    public function edit($nama)
+    public function edit($hari)
     {
-        $mapel = DB::table('Mapel')->where('Nama', $nama)->first();
+        $mapel = DB::table('Mapel')->where('Nama', $hari)->first();
         return view('mapel.edit', compact('mapel'));
     }
 
-    public function update(Request $request, $nama)
+    public function update(Request $request, $hari)
     {
         $request->validate([
-            'Nama' => 'required',
-            'Tugas1' => 'required',
-            'Tugas2' => 'required',
-            'Tugas3' => 'required',
-            'UTS' => 'required',
-            'Tugas4' => 'required',
-            'Tugas5' => 'required',
-            'Tugas6' => 'required',
-            'UAS' => 'required',
+            'Hari' => 'required',
+            'NamaMapel' => 'required',
+            'GuruPengampu' => 'required',
         ]);
-        Mapel::find($nama)->update($request->all());
+        Mapel::find($hari)->update($request->all());
         return redirect()->route('nama.index')
             ->with('success', 'Mata Pelajaran Berhasil Diupdate');
     }
 
-    public function destroy($nama)
+    public function destroy($hari)
     {
-        Mapel::find($nama)->delete();
+        Mapel::find($hari)->delete();
         return redirect()->route('mapel.index')
             ->with('success', 'Mata Pelajaran Berhasil Dihapus');
     }
